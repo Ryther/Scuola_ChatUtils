217fd01075bac3fa95cf04f791650823cb50d33f
Edoardo Zanoni <edoardo.zanoni@gmail.com>
2016-05-24 19:57:44 +0200

    Creato repository Scuola_ChatUtils da Scuola_Utils https://github.com/Ryther/Scuola_Utils.git

---
+++ Consts.java
@@ -0,0 +1,11 @@
+
+
+/**
+ *
+ * @author Edoardo Zanoni
+ */
+public class Consts {
+    
+    public static final int PORT = 1337;
+    public static final String dateFormat = "E dd/MM/yy-kk.mm";
+}
