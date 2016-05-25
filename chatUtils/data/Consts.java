b4634baae9acf189c5739707c64ab2e91a48dc42
Edoardo Zanoni <edoardo.zanoni@gmail.com>
2016-05-25 16:45:26 +0200

    Creati package per migliorare la gestione del progetto 3

---
+++ chatUtils/data/Consts.java
@@ -0,0 +1,15 @@
+package chatUtils.data;
+
+
+
+/**
+ *
+ * @author Edoardo Zanoni
+ */
+public class Consts {
+    
+    public static final int PORT = 1337;
+    public static final String dateFormat = "E dd/MM/yy-kk.mm";
+    
+    public static final int TALKER_THREADS = 2;
+}
