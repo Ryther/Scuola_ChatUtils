a8062d59c8c7530ec4f097aeee54027e571cfb29
Edoardo Zanoni <edoardo.zanoni@gmail.com>
2016-05-26 11:45:35 +0200

    [Consts.java]Aggiunta voce per thread-pool clients

--- chatUtils/data/Consts.java
+++ chatUtils/data/Consts.java
@@ -11,5 +11,6 @@ public class Consts {
     public static final int PORT = 1337;
     public static final String dateFormat = "E dd/MM/yy-kk.mm";
     
+    public static final int CLIENT_THREADS = 10;
     public static final int TALKER_THREADS = 2;
 }
