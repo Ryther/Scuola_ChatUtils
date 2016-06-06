d6a6da945e05c27db47c438b7f5b805bd77813c4
Edoardo Zanoni <edoardo.zanoni@gmail.com>
2016-06-06 12:00:36 +0200

    [Talker.java]Modifiche per l'utilizzo con SocketChannelHandler

--- chatUtils/net/Talker.java
+++ chatUtils/net/Talker.java
@@ -2,10 +2,12 @@ package chatUtils.net;
 
 
 import chatUtils.data.ChatMessage;
+import chatUtils.data.UserData;
 import java.nio.channels.SocketChannel;
 import java.util.Map;
 import java.util.Scanner;
 import java.util.concurrent.ConcurrentHashMap;
+import utils.net.SocketChannelHandler;
 
 /**
  * Talker: classe di appoggio per l'esecuzione in loop (polling) di metodi di 
@@ -24,25 +26,31 @@ public class Talker implements Runnable {
         READER, WRITER
     }
     
-    private final SocketChannel socketChannel; // Handler per la gestione degli stream.
-    private final TalkerType talkerType; // Enumeratorre di ripo.
+    
+    private final UserData userData; // Mappa per la  gestione degli oggetti da inviare.
     private final Map<TalkerType, Object> dataMap; // Mappa per la  gestione degli oggetti da inviare.
+    private final SocketChannelHandler socketChannelHandler; // Handler per la gestione degli stream.
+    private final TalkerType talkerType; // Enumeratorre di ripo.
     
     /**
     * Costruttore: inizializza Talker, usando un channel per inviare
     * e ricevere oggetti.
     * 
+    * @param userData L'<tt>UserData</tt> contenente le informazioni
+    * dell'utente.
     * @param dataMap Una <tt>ConcurentHashMap<TalkerType, Object></tt> 
     * contenente due valori, entrambi con chiave di tipo <tt>TalkerType</tt>
     * e valori di tipo <tt>Object</tt>.
-    * @param socketChannel Il SocketChannel di connessione al server.
+    * @param socketChannelHandler Il <tt>SocketChannelHandler</tt> di
+    * connessione al server.
     * @param talkerType La tipologia di <tt>Runnable</tt> che si vuole creare,
     * con stati selezionabili tra: {@link TalkerType}.
     */
-    public Talker(Map<TalkerType, Object> dataMap,SocketChannel socketChannel, TalkerType talkerType) {
+    public Talker(UserData userData, Map<TalkerType, Object> dataMap,SocketChannelHandler socketChannelHandlerù, TalkerType talkerType) {
         
+        this.userData = userData;
         this.dataMap = new ConcurrentHashMap();
-        this.socketChannel = socketChannel;
+        this.socketChannelHandler = socketChannelHandlerù;
         this.talkerType = talkerType;
     }
     
@@ -73,11 +81,13 @@ public class Talker implements Runnable {
         
         Scanner scanner = new Scanner(System.in);
         String message;
+        ChatMessage chatMessage = new ChatMessage(userData.getUserName());
+        chatMessage.setChatName("t");
         while(!((message = scanner.nextLine()).equals("stop"))) {
             
-            this.chatMessage.setMessage(message);
-            this.chatMessage.setDateTime();
-            socketChannel.pushToStream(this.chatMessage);
+            chatMessage.setMessage(message);
+            chatMessage.setDateTime();
+            socketChannelHandler.pushToChannel(chatMessage);
         }
     }
     
@@ -90,9 +100,9 @@ public class Talker implements Runnable {
     private void reader() {
         
         ChatMessage incomingMessage;
-        while(!((incomingMessage = (ChatMessage)socketChannel.pullFromStream()).getMessage().equals("stop"))) {
+        while(!((incomingMessage = (ChatMessage)socketChannelHandler.pullFromChannel()).getMessage().equals("stop"))) {
             
-            System.out.println(incomingMessage.getDateTime() + " - [" + incomingMessage.getUsername() + "] " + incomingMessage.getMessage());
+            System.out.println(incomingMessage);
         }
     }
 }
